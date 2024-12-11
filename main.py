e25e570cdc31a209bad6d8f05b2a3bbe995b7014
diviper <churendmitriy@gmail.com>
2024-12-11 13:26:46 +0300

    Fix import for DefaultBotProperties from aiogram.client.bot

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import asyncio
-from aiogram import Bot, Dispatcher, DefaultBotProperties
+from aiogram import Dispatcher
+from aiogram.client.bot import Bot, DefaultBotProperties
 from config import BOT_TOKEN
 from bot import router
 
 async def main():
-    # Указываем parse_mode через DefaultBotProperties
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
     dp = Dispatcher()
     dp.include_router(router)
 
-    # Удаляем старые апдейты и запускаем бота
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
